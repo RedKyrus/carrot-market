58b4433e3a7bdf165716ae06c1e7232bfcf2f48b
RedKyrus <ahehfldi2022@gmail.com>
2022-10-07 17:40:25 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## 캐럿마켓 클론코딩 NEXT_JS
 
+## https://planetscale.com/
---
+++ pages/api/client-test.tsx
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+
+
+export default async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse
+){
+  
+  // await client.user.create({
+  //   data: {
+  //     email: "hi",
+  //     name: "hi"
+  //   }
+  // });
+  await client.user.count();
+
+
+  res.json({
+    ok: true,
+    count: 
+  })
+
+
+}
