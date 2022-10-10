5e9f1776a5b762ae9606c50f8c852ce6ab13c5a7
RedKyrus <ahehfldi2022@gmai.com>
2022-10-11 01:45:04 +0900

    update

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,4 +1,26 @@
+import { useForm } from "react-hook-form"
 
 
-
-
+export default function Forme(){
+  const {register, handleSubmit} = useForm();
+  const onValid = () =>{
+    console.log("베리 굿~");
+  }
+  return(
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register("Username",{required:"true"})}
+        type="text" placeholder="Username" required
+      />
+      <input
+        {...register("email",{required:"true"})}
+        type="email" placeholder="Email" required
+      />
+      <input 
+        {...register("password",{required:"true"})}
+        type="password" placeholder="Password" required
+      />
+      <button type="submit" value="Create Account"></button>
+    </form>
+  )
+}
