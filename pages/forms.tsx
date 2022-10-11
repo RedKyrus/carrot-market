ae36274c854c8c31ae7b67bee67426da06771493
RedKyrus <ahehfldi2022@gmail.com>
2022-10-11 17:09:00 +0900

    update

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,26 +1,61 @@
-import { useForm } from "react-hook-form"
+import { useDebugValue } from "react";
+import { FieldErrors, useForm } from "react-hook-form"
+
+
+interface LoginForm{
+  username: string;
+  password: string;
+  email: string;
+}
 
 
 export default function Forme(){
-  const {register, handleSubmit} = useForm();
-  const onValid = () =>{
+  const {
+    register, 
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginForm>({
+    mode: "onChange"
+  });
+
+  const onValid = (data:LoginForm) =>{
     console.log("베리 굿~");
   }
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
+  }
+
   return(
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)  }>
       <input
-        {...register("Username",{required:"true"})}
-        type="text" placeholder="Username" required
+        {...register("username",{
+          required:"Username is required",
+          minLength: {
+            value: 5,
+            message: "테스트"
+          }
+          })}
+        type="text" placeholder="Username"
       />
+      
       <input
-        {...register("email",{required:"true"})}
-        type="email" placeholder="Email" required
+        {...register("email",{
+          required:"Email is required",
+          validate:{
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed"
+          }
+        })}
+        type="email" placeholder="Email"
+        className={`${Boolean(errors.email) ? "focus:ring-transparent border-red-500 focus:border-red-500" : ""}`}
       />
+      {errors.email?.message}
+
       <input 
-        {...register("password",{required:"true"})}
-        type="password" placeholder="Password" required
+        {...register("password",{required:"Password is required"})}
+        type="password" placeholder="Password" autoComplete="true"
       />
-      <button type="submit" value="Create Account"></button>
+      <button type="submit" value="">Create Account</button>
     </form>
   )
 }
