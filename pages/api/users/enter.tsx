50d68d997c79882a16e48347aa26afdd70aebbb8
RedKyrus <ahehfldi2022@gmail.com>
2022-10-14 17:57:12 +0900

    upadate

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,10 +3,33 @@ import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(401).end();
+  // if (req.method !== "POST") {
+  //   res.status(401).end();
+  // }
+  // console.log(req.body);
+
+  const { Phone, email } = req.body;
+  let user;
+
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      console.log("유저 만들어라!!");
+      user = await client.user.create({
+        data: {
+          name: "Aonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
   }
-  console.log(req.body);
+
   res.status(200).end();
 }
 
