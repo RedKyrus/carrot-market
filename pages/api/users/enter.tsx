4cc08a1a336443014e331de65018a0a444a9cd03
RedKyrus <ahehfldi2022@gmail.com>
2022-10-17 18:47:55 +0900

    update

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,34 +1,41 @@
 import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
+import { create } from "domain";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // if (req.method !== "POST") {
-  //   res.status(401).end();
-  // }
-  // console.log(req.body);
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: "anonymous",
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+  // console.log(user);
 
-  const { Phone, email } = req.body;
-  let user;
-
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      console.log("유저 만들어라!!");
-      user = await client.user.create({
-        data: {
-          name: "Aonymous",
-          email,
+  const token = await client.token.create({
+    data: {
+      payload: payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "anonymous",
+            ...user,
+          },
         },
-      });
-    }
-    console.log(user);
-  }
+      },
+    },
+  });
+  console.log(token);
 
   res.status(200).end();
 }
