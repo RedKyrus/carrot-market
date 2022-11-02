eb706fb9d654c73f5fb29069577c19a5b0965f11
RedKyrus <ahehfldi2022@gmail.com>
2022-11-02 18:22:59 +0900

    update

---
+++ pages/api/posts/[id]/answers.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+import { userAgent } from "next/server";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: Number(id),
+        },
+      },
+      answer,
+    },
+  });
+
+  res.json({
+    ok: true,
+    answer: newAnswer,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
